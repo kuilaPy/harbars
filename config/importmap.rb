34359f54373991e386e1b527eac7e585ee125ade
Prosenjit Chongder <prosenjit.chongder@geogo.in>
2024-05-27 12:29:36 +0530

    initial commit

---
+++ config/importmap.rb
@@ -0,0 +1,8 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "flowbite", to: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.turbo.min.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
