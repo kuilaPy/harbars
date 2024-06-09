403dcff520898030b2801985fdc63f52409f70fa
Avijit Kuila <avijitkuila5@gmail.com>
2024-06-09 14:44:29 +0530

    admin sign_up fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   devise_for :admin_users, path: 'admin', controllers: {
-    sessions: 'admin/sessions'
+    sessions: 'admin_users/sessions',
+    registrations: 'admin_users/registrations'
   }
   
   namespace :admin do
