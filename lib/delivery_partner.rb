0ecc7e5a20444a48a986a1bd98d67ea61f98e856
Avijit Kuila <avijitkuila5@gmail.com>
2024-07-10 23:22:13 +0530

    Merge branch 'master' of github.com:kuilaPy/harbars into avijit_branch-2

--- lib/delivery_partner.rb
+++ lib/delivery_partner.rb
@@ -19,7 +19,42 @@ module DeliveryPartner
     end
   end
 
-  def self.get_delivery_cost dest_pin
-    url = "#{ENV['DELIVERY_API_URL']}/api/kinko/v1/invoice/charges/.json"
+  def self.create_shipment data
+    url = "#{ENV['DELIVERY_BASE_URL']}/api/cmu/create.json"
+    begin
+      response = HTTParty.post(url, 
+        headers: {
+          'Content-Type' => 'application/json',
+          'Authorization' => "Token #{ENV['DELIVERY_API_KEY']}"
+        },
+        body: {
+          format: 'json',
+          data: data.to_json
+        }
+      )
+      data = response.parsed_response
+    rescue
+      return nil
+    end
+
+  end
+
+  def self.get_delivery_cost dest_pin, weight
+    url = "#{ENV['DELIVERY_BASE_URL']}/api/kinko/v1/invoice/charges/.json?md=S&ss=Delivered&o_pin=#{PIN}&d_pin=#{dest_pin}&cgm=#{weight}"
+    begin
+      response = HTTParty.get(url, headers: {
+        'Content-Type' => 'application/json',
+        'Authorization' => "Token #{ENV['DELIVERY_API_KEY']}"
+      })
+      data = response.parsed_response
+      if data.present? && data[0].present? && data[0]["total_amount"].present?
+        return data[0]["total_amount"]
+      else
+        return 0
+      end
+    rescue => e
+      puts e
+      return 0
+    end
   end
 end
