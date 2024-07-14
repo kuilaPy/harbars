a3dd9c73aac862756cdb079bce98acf02ce8572c
Avijit Kuila <avijitkuila5@gmail.com>
2024-07-14 21:51:34 +0530

    add admin order ui

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,10 +1,10 @@
 class Admin::BaseController < ActionController::Base
   before_action :authenticate_admin_user!
-  before_action :add_breadcrumbs
+  before_action :add_initial_breadcrumbs
   layout "admin"
 
 
-  def add_breadcrumbs
+  def add_initial_breadcrumbs
     breadcrumbs.add "Admin"
   end
 end
