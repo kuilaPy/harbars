d6844179a3cca53828e23220638f14e1ee4306d2
Prosenjit Chongder <prosenjit.chongder@geogo.in>
2024-07-21 19:28:33 +0530

    fixed external id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number, :external_user_id])
   end
 end
