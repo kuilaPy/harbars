764937129092f6160e861949eeff7ed62c138df1
Avijit Kuila <avijitkuila5@gmail.com>
2024-07-29 01:14:14 +0530

    change password add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :authenticate_user!
+  before_action :set_user, only: [:show, :edit, :update, :edit_password]
   before_action :add_breadcrumbs
   def show
+    breadcrumbs.add "Profile Information", user_path(current_user.id)
   end
 
   def edit
@@ -9,6 +11,18 @@ class UsersController < ApplicationController
 
   def update
     @user.update(user_params)
+  end
+
+  def edit_password
+    breadcrumbs.add "Change Password", edit_user_password_path(current_user.id)
+  end
+
+  def update_password
+    if current_user.update(password_params)
+      redirect_to root_path, notice: 'Password was successfully updated.'
+    else
+      render :edit_password
+    end
   end
 
   private
@@ -19,10 +33,12 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:email, :first_name, :last_name, :phone_number)
   end
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
 
   def add_breadcrumbs
     breadcrumbs.add "Home", :root_path
     breadcrumbs.add "Profile", :users_path
-    breadcrumbs.add "Profile Information", user_path(current_user.id)
   end
 end
