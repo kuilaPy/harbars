cfec4f16623f135d3c3d0ca8688595e0dc5f5d7c
Avijit Kuila <avijitkuila5@gmail.com>
2024-08-06 21:52:51 +0530

    drak mode and resposivenes bug fix

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -9,34 +9,3 @@ import "@rails/actiontext"
 import jQuery from "jquery"
 window.jQuery = jQuery
 window.$ = jQuery 
-
-console.log("hello from application.js -----------")
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('dom loaded...................................... ')
-  const toggleButton = document.getElementById('dark-mode-toggle');
-  const bodyElement = document.body;
-
-  // Apply dark mode based on localStorage
-  if (localStorage.getItem('theme') === 'dark' || 
-      (!localStorage.getItem('theme') && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    bodyElement.classList.add('dark');
-    console.log('dark mode on ======   for local storage =============================')
-  } else {
-    bodyElement.classList.remove('dark');
-    console.log('dark mode off ===== for local storage ============================')
-  }
-
-  // Toggle dark mode on button click
-  toggleButton.addEventListener('click', () => {
-    console.log('clicked=========...................')
-    bodyElement.classList.toggle('dark');
-    // Save the preference in localStorage
-    if (bodyElement.classList.contains('dark')) {
-      localStorage.setItem('theme', 'dark');
-      console.log('dark mode on ==========    for click============================================')
-    } else {
-      localStorage.setItem('theme', 'light');
-      console.log('dark mode off ========  for click =============')
-    }
-  });
-});
